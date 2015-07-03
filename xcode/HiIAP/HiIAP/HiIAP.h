706ec62b61ee70fbcaf87c8054b0b8ce629d590e
hiram <hiramtan@qq.com>
2015-07-03 14:05:48 +0800

    add Xcode project

---
+++ xcode/HiIAP/HiIAP/HiIAP.h
@@ -0,0 +1,22 @@
+//
+//  HiIAP.h
+//  HiIAP
+//
+//  Created by hiram on 15-7-3.
+//  Copyright (c) 2015年 hiram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HiIAP : NSObject
+
++(HiIAP*) instance;
+
+
+/*
+-(void) Init(void);
+void _Init();
+void _Purchase(StringPtr arg);
+*/
+
+@end
---
+++ xcode/HiIAP/HiIAP/HiIAP.m
@@ -0,0 +1,23 @@
+//
+//  HiIAP.m
+//  HiIAP
+//
+//  Created by hiram on 15-7-3.
+//  Copyright (c) 2015年 hiram. All rights reserved.
+//
+
+#import "HiIAP.h"
+
+@implementation HiIAP
+
+
+
+/*
+static HiIAP* gameMgr = nil;
+-(id)init
+{
+    
+}
+*/
+
+@end
